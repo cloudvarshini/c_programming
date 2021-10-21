f867d20ff732b0b5991c017d65d8738ca0ac1948
Megavarshini S <cloudvarshini@gmail.com>
2021-10-21 20:12:44 +0530

    Unterstanding the second type of conditional statement that is if-else

---
+++ if-else_statements.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+    int a = 5;
+    if (a>=10)
+    {
+        printf("Hello World");
+    }
+    else
+    {
+        printf("Welcome to learn C programme\n");
+    }
+    return 0;
+}
