4e49982b34842fe0126790f4be7659093fa172ef
Megavarshini S <cloudvarshini@gmail.com>
2021-11-24 20:46:13 +0530

    Without using fuctions,print the name twice if the input integer is even otherwise print the name once.

---
+++ even_odd_2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    char name[30];
+    printf("Enter the name:");
+    scanf("%s",name);
+    printf("Enter the number:");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("%s\n",name);
+        printf("%s\n",name);
+    }
+    else
+    {
+        printf("%s\n",name);
+    }
+}
