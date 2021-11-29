616802cf008a036bfd7f75062f45f32f3366533e
Megavarshini S <cloudvarshini@gmail.com>
2021-11-29 20:48:14 +0530

    my Tutorial

---
+++ Tutorial_4.c
@@ -0,0 +1,38 @@
+// 1. What is the value of a?
+ #include <stdio.h>
+#include <string.h>
+int main()
+{
+    int a;
+    a = strcmp("HEllo","Hello");
+    printf("%d\n",a);
+    return 0;
+}
+
+
+/*// 2. Find the output of the following code:
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int *p;
+    p = &a;
+    printf("%d\n",*p+1);
+    return 0;
+}
+
+
+
+// 3. Find the output of the below programme:
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int a[2] = {1,2};
+    int *b;
+    b = a;
+    printf("%d\n",*b);
+    return 0;
+}
+*/
